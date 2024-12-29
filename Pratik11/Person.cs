2db0bb5e205b4e2fc30f647807d1fcdcf398c286
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-12-30 01:29:25 +0300

    Add project files.

---
+++ Pratik11/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Pratik11
+{
+    public class Person
+    {
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public bool IsStudent { get; set; }
+        public bool IsTeacher => !IsStudent;
+
+        public void DisplayProperties()
+        {
+            Console.WriteLine($"Merhaba benim adım {Name} {Surname}. Ben bir {(IsStudent ? "öğrenciyim" : "öğretmenim")}");
+        }
+    }
+}
